72173d2d8a7b929c02ae35da5cd0e397c8058de6
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2023-02-04 09:44:38 -0300

    Restart environment configuration

--- api/config/bundles.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
-    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
-];
